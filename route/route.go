91141a9daf8ca4af3dbcbdb8f87430db54243c6d
kindevil <kindevil@outlook.com>
2020-12-08 14:24:17 +0800

    update

--- route/route.go
+++ route/route.go
@@ -1,6 +1,6 @@
 /*
  * @Author: jia
- * @LastEditTime: 2020-12-08 14:01:09
+ * @LastEditTime: 2020-12-08 14:21:23
  * @FilePath: /pi-monitor/route/route.go
  * @Date: 2020-12-06 19:58:05
  * @Software: VS Code
@@ -10,6 +10,7 @@ package route
 import (
 	"net/http"
 	"pi-monitor/api"
+	"pi-monitor/service"
 
 	"github.com/gin-gonic/gin"
 )
@@ -18,8 +19,19 @@ func route() {
 	server.Gin.LoadHTMLGlob("views/**/*")
 	server.Gin.Static("static/", "static/")
 
+	host := service.GetHost()
+
 	data := gin.H{}
 	data["title"] = "Raspberrypi Monitor"
+	data["boottime"] = host.BootTime
+	data["version"] = host.Version
+	data["serial"] = host.Serial
+	data["hostname"] = host.Hostname
+	data["platform"] = host.Platform
+	data["kernal"] = host.Kernal
+	data["hardware"] = host.Hardware
+	data["model"] = host.Model
+	data["os"] = host.OS
 
 	server.Gin.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "home.tmpl", data)
