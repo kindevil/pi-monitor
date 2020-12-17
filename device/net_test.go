6c2dd77eccd821ff4144cc66e9027acc1b0160d2
kindevil <kindevil@outlook.com>
2020-12-17 11:04:21 +0800

    update

---
+++ device/net_test.go
@@ -0,0 +1,18 @@
+/*
+@Time : 2020/12/17 10:58 上午
+@Author : jia
+@File : net_test.go.go
+@Software : GoLand
+*/
+
+package device
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestGetNetNames(t *testing.T) {
+	lastInterface = interfaceStat()
+	fmt.Println(GetNetNames())
+}
